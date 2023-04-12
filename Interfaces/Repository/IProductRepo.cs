2ee089547adf8dadedb69e8632aa6841f8239cb3
Rahat <rahatsarker40@gmail.com>
2023-04-12 16:52:37 +0600

    S

---
+++ Interfaces/Repository/IProductRepo.cs
@@ -0,0 +1,10 @@
+﻿using EF.Core.Repository.Interface.Manager;
+using EF.Core.Repository.Interface.Repository;
+using RepositoryPattern.Models;
+
+namespace RepositoryPattern.Interfaces.Repository
+{
+    public interface IProductRepo:ICommonRepository<Product>
+    {
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RepositoryPattern.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter item name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter item description")]
+        public string Descriptions { get; set; }
+        [Required(ErrorMessage = "Please enter item price")]
+        public string Price { get; set; }
+    }
+}
